7970598a10bf1ae847ea7f920a3ab35c3e706aab
ulfahasanah <ulfa2912@outlook.com>
2020-03-20 08:37:17 +0700

    set multiplayer, edit app.js for multiplayer, edit socketio to io

--- app.js
+++ app.js
@@ -1,54 +1,74 @@
 const express = require ('express') ;
 const app = express() ;
 const http = require('http').createServer(app) ;
-const socketio = require('socket.io')(http) ;
+const io = require('socket.io')(http) ;
 const PORT = 3000 ;
 
-let positions= []
-let winner = false
+let positions= [
+    {
+      x: 340,
+      y: 620,
+    },
+    {
+      x: 100,
+      y: 620
+    }
+  ]
 
-socketio.on('connection', (socket) => {
+let winner = true
+let idFirst;
+let idSecond;
+
+io.on('connection', socket => {
+    socket.emit('positions', positions);
     socket.on('move', data => {
-        positions.push(data)
-        console.log(positions);
-        if (data.y <= 0) {
-            winner = true
-            socketio.emit('winner', true)
-            console.log('yeay winners');
+
+        if(!idFirst && !idSecond) {
+            idFirst = data.id
+            positions[0].y -=data.dice;
+            if (positions[0].y <=0 ) {
+                positions[0].y = 0
+                io.emit('positions', positions)
+                io.emit('winner', winner)
+            } else {
+                io.emit('positions', positions)
+            }
+            
+        } else if(idFirst && !idSecond && idFirst !== data.id) {
+            positions[1].y -=data.dice;
+                if (positions[1].y <=0 ) {
+                    positions[1].y = 0
+                    io.emit('positions', positions)
+                    io.emit('winner', winner)
+                } else {
+                    io.emit('positions', positions)
+                }
+
+        } else if(idFirst && idSecond) {
+
         }
+            if (idFirst === data.id) {
+                positions[0].y -=data.dice;
+                if (positions[0].y <=0 ) {
+                    positions[0].y = 0
+                    io.emit('positions', positions)
+                    io.emit('winner', winner)
+                } else {
+                    io.emit('positions', positions)
+                }
+            } else if(idSecond === data.id){
+                positions[1].y -=data.dice;
+                if (positions[1].y <=0 ) {
+                    positions[1].y = 0
+                    io.emit('positions', positions)
+                    io.emit('winner', winner)
+                } else {
+                    io.emit('positions', positions)
+                }
+            }
 
     })
-    // players.push(1)
-    // console.log(players);
-    // if (players.length === 2) {
-    //     socket.emit('positions', positions);
-    //     socket.emit('index', index);
-    //     socket.on('move', data => {
-    //         if (data.index %2 === 0) {
-    //             index +=1
-    //             socketio.emit('index', index);
-    //             positions[0].y -=data.dice;
-    //             if (positions[0].y <=0 ) {
-    //                 positions[0].y = 0
-    //                 socketio.emit('positions', positions)
-    //                 socketio.emit('winner', winner)
-    //             } else {
-    //                 socketio.emit('positions', positions)
-    //             }
-    //         } else {
-    //             index +=1
-    //             socketio.emit('index', index);
-    //             positions[1].y -=data.dice;
-    //             if (positions[1].y <=0 ) {
-    //                 positions[1].y = 0
-    //                 socketio.emit('positions', positions)
-    //                 socketio.emit('winner', winner)
-    //             } else {
-    //                 socketio.emit('positions', positions)
-    //             }
-    //         }
-    //     })
-    // }
+
 })
 
 http.listen(PORT, () => {
