f83541e8492d11f551a5a70821e55fddccf90f80
MickeyStrike <irfanmaulana281299@gmail.com>
2020-03-20 13:22:14 +0700

    test deploy

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require ('express') ;
 const app = express() ;
 const http = require('http').createServer(app) ;
 const io = require('socket.io')(http) ;
-const PORT = 3000 ;
+// const PORT = 3000 ;
+const PORT = process.env.PORT || 3000
 
 let positions= [
     {
